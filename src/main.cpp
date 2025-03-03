2076c0421d3c30466b4cb5e3d59d03b075b066fe
Mike <45664417+thewhobox@users.noreply.github.com>
2025-03-03 13:01:33 +0100

    added: Master ApplicationLayer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "Arduino.h"
-#include "Dali/DataLinkLayer.h"
+#include "Dali/Master.h"
 
-Dali::DataLinkLayer dll1;
-Dali::DataLinkLayer dll2;
+Dali::Master m1;
+Dali::Master m2;
 
 uint32_t inttime = 0;
 uint8_t tdata;
@@ -51,60 +51,36 @@ void setup()
     delay(1000);
     printf("Setup\n");
 #ifdef ARDUINO_ARCH_ESP32
-    dll1.init(7, 20);
-    dll2.init(14, 13);
+    m1.init(7, 20);
+    m2.init(14, 13);
 #endif
 #ifdef ARDUINO_ARCH_RP2040
-    dll1.init(17, 16);
-    dll2.init(17, 26);
+    m1.init(17, 16);
+    m2.init(17, 26);
 #endif
-    dll2.registerMonitor(onDaliFrame2);
-    dll1.registerMonitor(onDaliFrame1);
+    m1.registerMonitor(onDaliFrame2);
+    m2.registerMonitor(onDaliFrame1);
     printf("Setup done\n");
 }
 uint32_t t = 0;
+uint8_t arc = 0;
 void loop()
 {
-    dll1.process();
-    dll2.process();
+    m1.process();
+    m2.process();
 
-    // if (millis() - t > 5000)
-    // {
-    //     printf("loop\n");
-    //     t = millis();
-    // }
     if (millis() - lasttx > 2000)
     {
         lasttx = millis();
+        if(arc == 0)
+            arc = 0xFE;
+        else
+            arc = 0;
         // printf("\n\nSend test frames\n\n");
-        Dali::Frame txFrame;
-        txFrame.flags = DALI_FRAME_FORWARD;
-        txFrame.ref = micros();
-        txFrame.data = 0x000010FF;
-        txFrame.size = 16;
-        dll1.transmitFrame(txFrame);
+        
         for (size_t i = 0; i < 3; i++)
         {
-            txFrame.data = 0x000000A0 + i;
-            txFrame.size = 8;
-            txFrame.ref = micros();
-            dll1.transmitFrame(txFrame);
+            m1.sendArc(i, arc);
         }
-
-        // txFrame.data = (micros() + 1) & 0x0000FFFF;
-        // txFrame.size = 16;
-        // dll1.transmitFrame(txFrame);
-        // txFrame.data = (micros() + 1) & 0x0000FFFF;
-        // txFrame.size = 16;
-        // dll1.transmitFrame(txFrame);
-        // txFrame.data = (micros() + 1) & 0x0000FFFF;
-        // txFrame.size = 16;
-        // dll1.transmitFrame(txFrame);
-        // txFrame.data = (micros() + 1) & 0x0000FFFF;
-        // txFrame.size = 16;
-        // dll1.transmitFrame(txFrame);
-        // txFrame.data = 0x3030;
-        // txFrame.size = 16;
-        // dll1.transmitFrame(txFrame);
     }
 }
