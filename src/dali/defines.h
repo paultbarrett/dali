f691957cc67b3f67d729b6ea6fec63bd6cb1196c
Marco Scholl <develop@marco-scholl.de>
2025-03-16 11:45:00 +0100

    remove unused DALI threshold definitions from defines.h

--- src/dali/defines.h
+++ src/dali/defines.h
@@ -4,8 +4,3 @@
 #define DALI_TE_TO_MS(x) ((uint)(x * DALI_TE / 1000))
 #define DALI_TE_TO_US(x) ((uint)(x * DALI_TE))
 #define DALI_RX_BITS 128 // 64 + 1 Start + 2 Stopbits - 128 is next valid value
-
-#define DALI_THRESHOLD_1TE_LOW ((uint)(DALI_TE * 0.8))
-#define DALI_THRESHOLD_1TE_HIGH ((uint)(DALI_TE * 1.2))
-#define DALI_THRESHOLD_2TE_LOW ((uint)(DALI_TE + DALI_THRESHOLD_1TE_LOW))
-#define DALI_THRESHOLD_2TE_HIGH ((uint)(DALI_TE + DALI_THRESHOLD_1TE_HIGH))
